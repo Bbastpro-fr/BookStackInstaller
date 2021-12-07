a18a3244d795ca78c2135d6e8812f1be5890b792
Bbastpro <proust.bastien@gmail.com>
2021-12-07 22:13:24 +0100

    Update ComposerInstaller.sh

--- ComposerInstaller.sh
+++ ComposerInstaller.sh
@@ -6,7 +6,7 @@
 
 install(){
   echo "Install composer on your system ..."
-  $(check_if_php)
+  check_if_php
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   php composer-setup.php --install-dir=/usr/local/bin --filename=composer
   php -r "unlink('composer-setup.php');"
